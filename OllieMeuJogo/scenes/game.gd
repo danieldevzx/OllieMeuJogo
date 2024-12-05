996f56251ab1beee7e844dcb543932a59caeaa05
Daniel <danielmartinsdev@gmail.com>
2024-12-05 18:29:01 -0300

    vai com deus

--- OllieMeuJogo/scenes/game.gd
+++ OllieMeuJogo/scenes/game.gd
@@ -4,37 +4,42 @@ var songs = 0
 var songsPerSeconds = 0
 @onready var songLabel = $Songs
 @onready var songPerSLabel = $SongsPerSecond
-@onready var som = $Kick/AudioStreamPlayer2D
+@onready var soundKick = $Kick/AudioStreamPlayer2D
+@onready var soundSnare = $Caixa/Snare1
 @onready var timer = $Timer
 @onready var textureButton = $Kick/TextureButton
 
-var is_pressed = false  # Estado atual do botão
+var is_pressed = false
+
 
 func _ready() -> void:
 	update_labels()
 
-func _on_timer_timeout() -> void:
-	songs += songsPerSeconds
-	update_labels()
-	som.play()
-	toggle_texture_button_animation()
-
-func _on_texture_button_pressed() -> void:
-	songs += 10
-	update_labels()
-	som.play()
 
 func _on_upgrade_pressed() -> void:
 	if songs >= 100:
 		songs -= 100
-		songsPerSeconds += 10
 		timer.start()
 		update_labels()
 
 func update_labels() -> void:
 	songLabel.text = "Songs: " + str(songs)
-	songPerSLabel.text = "Songs/s: " + str(songsPerSeconds)
 
 func toggle_texture_button_animation() -> void:
 	is_pressed = !is_pressed
-	#textureButton.pressed = is_pressed
+	if is_pressed:
+		textureButton.pressed
+	else:
+		textureButton.pressed = is_pressed
+
+
+func _on_kick_pressed() -> void:
+		songs += 10
+		update_labels()
+		soundKick.play()
+
+
+func _on_snare_pressed() -> void:
+		songs += 10
+		update_labels()
+		soundSnare.play()
